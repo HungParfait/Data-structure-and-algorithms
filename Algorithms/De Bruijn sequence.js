3ac5ec54d3ffdbd413e33cfd060ba865706efbc1
HungParfait <81432965+HungParfait@users.noreply.github.com>
2022-04-10 20:39:20 +0700

    4/10/2022

---
+++ Algorithms/De Bruijn sequence.js
@@ -0,0 +1,56 @@
+const S = ['0', '1']
+
+function lyndonGenerator(wordLength, ) {
+    let result = ['0'],
+        w = '0',
+        lengthS = S.length
+
+    while (w !== S[lengthS - 1]) {
+        let length = w.length,
+            index = 0
+
+        while (length < wordLength) {
+            w += w[index % length]
+            index++
+            length = w.length
+        }
+
+        for (let i = (wordLength - 1); i >= 0; i--) {
+            if (w[i] !== S[lengthS - 1]) {
+                break
+            } else {
+                w = w.slice(0, -1)
+            }
+        }
+
+        let indexValue = S.indexOf(w[w.length - 1]) + 1
+        w = w.slice(0, -1) + S[indexValue]
+
+        result.push(w)
+    }
+
+    return result
+}
+
+
+function deBruijnSequence(n, ) {
+    let value = Math.floor(Math.sqrt(n)),
+        resultString = '',
+        divisor = []
+    let allLyndon = lyndonGenerator(n)/* .sort((str1, str2) => str1.length - str2.length); */
+    for (let i = 1; i <= value; i++) {
+        if (n % i === 0) {
+            divisor.push(i)
+        }
+    }
+    divisor.push(n)
+    for (let index = 0; index < allLyndon.length; index++) {
+        if (divisor.indexOf(allLyndon[index].length) >= 0) {
+            resultString += allLyndon[index]
+        }
+    }
+
+    return resultString
+}
+
+console.log(deBruijnSequence(4))
--- Algorithms/Lyndon Word.js
+++ Algorithms/Lyndon Word.js
@@ -1,31 +1,34 @@
-const S = ['0', '1'],
-    n = 5,
-    lengthS = S.length
-let result = ['0'],
-    w = '0'
+const S = ['0', '1']
 
-while (w !== S[lengthS - 1]) {
-    let length = w.length,
-        index = 0
+function lyndonGenerator(wordLength, ) {
+    let result = ['0'],
+        w = '0', lengthS = S.length
 
-    while (length < n) {
-        w += w[index % length]
-        index++
-        length = w.length
-    }
+    while (w !== S[lengthS - 1]) {
+        let length = w.length,
+            index = 0
 
-    for (let i = (n - 1); i >= 0; i--) {
-        if (w[i] !== S[lengthS - 1]) {
-            break
-        } else {
-            w = w.slice(0, -1)
+        while (length < wordLength) {
+            w += w[index % length]
+            index++
+            length = w.length
         }
+
+        for (let i = (wordLength - 1); i >= 0; i--) {
+            if (w[i] !== S[lengthS - 1]) {
+                break
+            } else {
+                w = w.slice(0, -1)
+            }
+        }
+
+        let indexValue = S.indexOf(w[w.length - 1]) + 1
+        w = w.slice(0, -1) + S[indexValue]
+
+        result.push(w)
     }
 
-    let indexValue = S.indexOf(w[w.length - 1]) + 1
-    w = w.slice(0, -1) + S[indexValue]
-
-    result.push(w)
+    return result
 }
 
-console.log(result)
+console.log(lyndonGenerator(5))
